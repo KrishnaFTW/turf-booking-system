3780ed0639a74af46c8e47badf8b3e801e079b32
Krishna Ranode <krishnaranode@gmail.com>
2026-08-08 01:08:59 +0530

    demo data in slots

---
+++ frontend/src/data/slots.js
@@ -0,0 +1,36 @@
+const slots = [
+  {
+    id: 1,
+    startTime: "06:00 AM",
+    endTime: "07:00 AM",
+    sport: "Cricket",
+    price: 600,
+    status: "AVAILABLE",
+  },
+  {
+    id: 2,
+    startTime: "07:00 AM",
+    endTime: "08:00 AM",
+    sport: "Football",
+    price: 600,
+    status: "BOOKED",
+  },
+  {
+    id: 3,
+    startTime: "08:00 AM",
+    endTime: "09:00 AM",
+    sport: "Cricket",
+    price: 800,
+    status: "AVAILABLE",
+  },
+  {
+    id: 4,
+    startTime: "09:00 AM",
+    endTime: "10:00 AM",
+    sport: "Football",
+    price: 800,
+    status: "AVAILABLE",
+  },
+];
+
+export default slots;
